137ca1b8022b67f1479c32844c5d10c302dc0e5a
mAbshn <m_abshn@mail.ru>
2021-09-25 22:43:30 +0300

    final

--- #src/js/script.js
+++ #src/js/script.js
@@ -2,12 +2,21 @@
     const burger = document.querySelector('.header__burger');
     const menu = document.querySelector('.header__nav');
     const btns = document.querySelector('.header__btns');
+    const list = document.querySelectorAll('.header__nav li');
     burger.addEventListener('click', () => {
         menu.classList.toggle('_active');
         btns.classList.toggle('_active');
         burger.classList.toggle('_active');
         document.body.classList.toggle('_noscroll');
     })
+    for (let li of list) {
+        li.addEventListener('click', () => {
+            menu.classList.toggle('_active');
+            btns.classList.toggle('_active');
+            burger.classList.toggle('_active');
+            document.body.classList.toggle('_noscroll');
+        })
+    }
 })();
 
 (function swiperjs() {
@@ -35,25 +44,10 @@
 })();
 
 (function footer() {
-    const chapter1 = document.querySelector('#chapter1');
-    const chapter2 = document.querySelector('#chapter2');
-    const chapter3 = document.querySelector('#chapter3');
-    const list1 = document.querySelectorAll('#chapter1 > li');
-    const list2 = document.querySelectorAll('#chapter2 > li');
-    const list3 = document.querySelectorAll('#chapter3 > li');
-    chapter1.addEventListener('click', () => {
-        for (let item of list1) {
-            item.classList.toggle('_active');
-        }
-    })
-    chapter2.addEventListener('click', () => {
-        for (let item of list2) {
-            item.classList.toggle('_active');
-        }
-    })
-    chapter3.addEventListener('click', () => {
-        for (let item of list3  ) {
-            item.classList.toggle('_active');
-        }
-    })
+    const chapters = document.querySelectorAll('.footer__chapter');
+    for (let chapter of chapters) {
+        chapter.addEventListener('click', () => {
+            chapter.classList.toggle('_active')
+        })
+    }
 })();
